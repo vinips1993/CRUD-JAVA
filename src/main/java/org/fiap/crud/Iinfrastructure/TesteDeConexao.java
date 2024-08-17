3b1dfec35deda6e4854e785dc6ef3a424039f381
vinips1993 <rm550907@fiap.com.br>
2024-08-17 20:14:14 -0300

    Criação do pacote infrastructure e Query

---
+++ src/main/java/org/fiap/crud/Iinfrastructure/TesteDeConexao.java
@@ -0,0 +1,41 @@
+package org.fiap.crud.Iinfrastructure;
+
+import java.sql.*;
+
+public class TesteDeConexao {
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            Connection conexao = DriverManager.getConnection(
+                    "jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL",
+                    "RM550907",
+                    "220493");
+
+            Statement stmt = conexao.createStatement();
+            String sql = "INSERT INTO T_PRODUTO (CD_PRODUTO, NM_PRODUTO, DT_VALIDADE, VL_PRODUTO) " +
+                    "VALUES (seq_id_produto.NEXTVAL, 'UVA', TO_DATE('18/08/2024', 'dd/mm/yyyy'), 12.80 )";
+            stmt.executeUpdate(sql);
+
+            ResultSet result = stmt.executeQuery("SELECT * FROM T_PRODUTO");
+
+            while (result.next()) {
+                System.out.println( result.getInt("CD_PRODUTO") + " " +
+                                    result.getString("NM_PRODUTO") + " " +
+                                    result.getDate("DT_VALIDADE") + " " +
+                                    result.getDouble("VL_PRODUTO"));
+            }
+
+            String sqlDelete = "DELETE FROM T_PRODUTO WHERE CD_PRODUTO =1";
+            stmt.executeUpdate(sqlDelete);
+
+            conexao.close();
+        } catch (SQLException e) {
+            System.err.println("Não foi possível conectar ao Oracle");
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            System.err.println("O Driver JDBC não foi encontrado");
+            e.printStackTrace();
+        }
+    }
+}
